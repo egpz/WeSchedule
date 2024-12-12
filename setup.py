6a54ec410f8ef0f5c217e00a20a2de6f0296be74
egpz <egluigi10@gmail.com>
2024-12-12 13:55:09 -0500

    fixing setup documentation

--- setup.py
+++ setup.py
@@ -6,13 +6,22 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pandas',
+        'google-api-python-client',
     ],
     entry_points={
         'console_scripts': [
             'study-group-scheduler=scheduler.scheduler:suggest_time',
         ],
     },
-    description='A tool to schedule study group sessions based on availability.',
-    author='egpz',
+    description='A tool to schedule group study sessions and create calendar events.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
     license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
